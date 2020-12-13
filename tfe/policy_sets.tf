3ff0a5edaca330b015700d6048c2167844904e57
kuroseets <etsuo.kurose@gmail.com>
2020-12-13 16:53:09 +0900

    見積もりポリシー変更

--- tfe/policy_sets.tf
+++ tfe/policy_sets.tf
@@ -19,6 +19,21 @@ resource "tfe_policy_set" "cost_estimate" {
   organization  = var.tfe_organization
   policies_path = "/policy_sets/cost_estimate"
   workspace_ids = [
+  ]
+
+  vcs_repo {
+    oauth_token_id     = var.tfe_oauth_id
+    branch             = "main"
+    identifier         = "kuroseets/qiita_pac_20201214"
+    ingress_submodules = false
+  }
+}
+
+resource "tfe_policy_set" "cost_estimate_test" {
+  name          = "cost_estimate_test"
+  organization  = var.tfe_organization
+  policies_path = "/policy_sets/cost_estimate"
+  workspace_ids = [
     tfe_workspace.qiita_pac_aws.id
   ]
 
@@ -28,4 +43,10 @@ resource "tfe_policy_set" "cost_estimate" {
     identifier         = "kuroseets/qiita_pac_20201214"
     ingress_submodules = false
   }
+}
+
+resource "tfe_policy_set_parameter" "cost_estimate_test_limit" {
+  key           = "limit"
+  value         = "50"
+  policy_set_id = tfe_policy_set.cost_estimate_test
 }
