32cbb2ac5a507432a548b8918fa256ace17386e3
kuroseets <etsuo.kurose@gmail.com>
2020-12-13 16:46:55 +0900

    ポリシー追加用コード

---
+++ tfe/policy_sets.tf
@@ -0,0 +1,31 @@
+resource "tfe_policy_set" "s3_policy_set" {
+  name          = "s3_policy_set"
+  organization  = var.organization
+  policies_path = "/policy_sets/s3_policy_set"
+  workspace_ids = [
+    tfe_workspace.qiita_pac_aws.id
+  ]
+
+  vcs_repo {
+    oauth_token_id     = var.tfe_oauth_id
+    branch             = "main"
+    identifier         = "kuroseets/qiita_pac_20201214"
+    ingress_submodules = false
+  }
+}
+
+resource "tfe_policy_set" "cost_estimate" {
+  name          = "cost_estimate"
+  organization  = var.organization
+  policies_path = "/policy_sets/cost_estimate"
+  workspace_ids = [
+    tfe_workspace.qiita_pac_aws.id
+  ]
+
+  vcs_repo {
+    oauth_token_id     = var.tfe_oauth_id
+    branch             = "main"
+    identifier         = "kuroseets/qiita_pac_20201214"
+    ingress_submodules = false
+  }
+}
