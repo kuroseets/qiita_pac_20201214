bbe803771d6445560c2fb54e983b2252e37862fb
kuroseets <etsuo.kurose@gmail.com>
2020-12-13 16:39:59 +0900

    テスト用コード変更、diff追加

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web_servers" {
   count         = length(var.availability_zones)
   ami           = "ami-0053d11f74e9e7f52"
-  instance_type = "t3.micro"
+  instance_type = "m5.large"
   subnet_id     = aws_subnet.private_subnet[count.index].id
   tags = {
     Name = format("%s-%s-web-server-%s", var.application_name, var.environment, var.availability_zones[count.index])
